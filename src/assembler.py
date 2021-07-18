c564bb026699d414e1d5916e16eccfb356af7189
d0rj <hanaan@internet.ru>
2021-07-18 17:16:48 +0300

    Now arguments type checking works with inner dict representation.

--- src/assembler.py
+++ src/assembler.py
@@ -24,6 +24,14 @@ _labels: Dict[str, int] = dict()
 _memory = bytearray(MEMORY_SIZE)
 
 
+def _first_key(var: dict):
+	return list(var.keys())[0]
+
+
+def _first_value(var: dict):
+	return list(var.values())[0]
+
+
 def _extract_label_name(tree: Tree) -> str:
 	return str(tree.children[0])
 
@@ -90,11 +98,11 @@ def _argument_count_missmatch_message(instruction_name: str, required: int, pass
 	return f'\'{instruction_name.upper()}\' instruction must have {required} arguments, but {passed} was gived.'
 
 
-def _arguments_matched(arguments: List[Tree], types: List[str], instr: str) -> Tuple[bool, str]:
+def _arguments_matched(arguments: List[dict], types: List[str], instr: str) -> Tuple[bool, str]:
 	if len(arguments) != len(types):
 		return False, _argument_count_missmatch_message(instr, len(types), len(arguments))
 
-	all_matched = all([_argument_type(arg) == _type for arg, _type in zip(arguments, types)])
+	all_matched = all([_first_key(arg) == _type for arg, _type in zip(arguments, types)])
 
 	if not all_matched:
 		return False, _argument_type_missmatch_message(instr, str(types).replace('[', '').replace(']', ''))
@@ -102,7 +110,7 @@ def _arguments_matched(arguments: List[Tree], types: List[str], instr: str) -> T
 	return True, str()
 
 
-def _arguments_matched_any(arguments: List[Tree], types: List[List[str]], instr: str) -> Tuple[int, str]:
+def _arguments_matched_any(arguments: List[dict], types: List[List[str]], instr: str) -> Tuple[int, str]:
 	for i, _types in enumerate(types):
 		matched, message = _arguments_matched(arguments, _types, instr)
 		if matched:
@@ -111,7 +119,7 @@ def _arguments_matched_any(arguments: List[Tree], types: List[List[str]], instr:
 	return -1, message
 
 
-def _required_argument_types(arguments: List[Tree], types: List[str], instr: str):	
+def _required_argument_types(arguments: List[dict], types: List[str], instr: str):	
 	matched, message = _arguments_matched(arguments, types, instr)
 	if not matched:
 		raise SyntaxError(message)
@@ -135,15 +143,13 @@ def preprocess(tree: Tree) -> Dict[int, Dict[str, list]]:
 			_labels[_extract_label_name(command)] = current_address
 			current_address -= 1
 		elif _is_orig_command(command):
-			arguments = _get_arguments_tree(command)
+			arguments = [_argument_tree_parse(arg_tree) for arg_tree in _get_arguments_tree(command)]
 			_required_argument_types(arguments, [NUMBER], ORIG)
-
-			current_address = _number_tree_to_int(arguments[0]) - 1
+			current_address = _first_value(arguments[0]) - 1
 		elif _is_fill_command(command):
-			arguments = _get_arguments_tree(command)
+			arguments = [_argument_tree_parse(arg_tree) for arg_tree in _get_arguments_tree(command)]
 			_required_argument_types(arguments, [NUMBER], FILL)
-
-			_memory[current_address] = _number_tree_to_int(arguments[0])
+			_memory[current_address] = _first_value(arguments[0])
 		# TODO: stringz and blkw
 		#else:
 			#raise SyntaxError(f'Unknown command: {command.children[0]}.')
@@ -157,7 +163,7 @@ def preprocess(tree: Tree) -> Dict[int, Dict[str, list]]:
 		# replace labels with address
 		arguments = [
 			{ 'number': _labels[list(arg.values())[0]] } 
-				if list(arg.keys())[0] == LABEL 
+				if _first_key(arg) == LABEL 
 				else arg 
 			for arg in arguments
 		]
