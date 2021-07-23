b076ac99e07077165a133010cdf5328dfa2f9817
d0rj <hanaan@internet.ru>
2021-07-23 16:23:01 +0300

    Documentation: changed docstring style, complying with the PEP requirements.

--- src/asm_code/argument.py
+++ src/asm_code/argument.py
@@ -5,35 +5,43 @@ from ..utils.lc3_constants import TokenType as TT
 
 
 class Argument:
-    """A class to represent an assembler instruction  argument.
+    """
+    A class to represent an assembler instruction  argument.
 
     Essentially, it's util class for two fields aggregation.
 
     Attributes:
-        type_ (str): An argument type. Can be one of :class:`utils.lc3_constants.TokenType`.
+        type_ (str): An argument type.
+            Can be one of :class:`utils.lc3_constants.TokenType`.
         value (any): Pythonic representation of argument.
 
     Methods:
         static :meth:`fromTree`.
     """
+
     __slots__ = 'type_', 'value'
 
     def __init__(self, type_: str, value) -> None:
-        """Instruction's argument constructor.
+        """
+        Instruction's argument constructor.
 
         Note:
-            It is recommended not to use it directly, 
-            but to create objects of this class using a static method :meth:`Argument.fromTree`.
+            It is recommended not to use it directly,
+            but to create objects of this class
+            using a static method :meth:`Argument.fromTree`.
 
         Args:
-            type_ (str): An argument type. Can be one of :class:`utils.lc3_constants.TokenType`.
+            type_ (str): An argument type.
+                Can be one of :class:`utils.lc3_constants.TokenType`.
             value (any): Pythonic representation of an argument.
         """
+
         self.type_ = type_
         self.value = value
 
     def __str__(self) -> str:
-        """String representation of argument.
+        """
+        String representation of argument.
 
         Returns:
             str: String representation in format <Type: ...; Value: ...>
@@ -42,25 +50,30 @@ class Argument:
             >>> str(Argument('number', 42))
             <Type: number; Value: 42>
         """
+
         return f'<Type: {self.type_}; Value: {self.value}>'
 
     def __repr__(self) -> str:
-        """Simple `__repr__` override.
+        """
+        Simple `__repr__` override.
 
         Based on :meth:`__str__` private method and returns the same.
         """
+
         return self.__str__()
 
     @classmethod
     def fromTree(cls, argument: Tree):
-        """Static fabric from :class:`lark.Tree` object.
+        """
+        Static fabric from :class:`lark.Tree` object.
 
         Args:
-            argument (lark.Tree): Tree object with argument representation, builded from parser.
+            argument (lark.Tree): Tree object with argument representation,
+            builded from parser.
 
         Returns:
-            (Argument | None): Returns constructed :class:`Argument` object; or
-                `None` if argument type not supported.
+            (Argument | None): Returns constructed :class:`Argument` object;
+            or `None` if argument type not supported.
 
         Example:
             >>> str(example_tree)
@@ -69,6 +82,7 @@ class Argument:
             >>> str(argument)
             <Type: number; Value: 12288>
         """
+
         arg_type = argument_type(argument)
 
         if arg_type == TT.NUMBER:
