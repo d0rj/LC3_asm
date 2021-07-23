2ffc39d2c561d48d3ff7a7f443fd058472c8aecb
d0rj <hanaan@internet.ru>
2021-07-23 17:41:36 +0300

    Main code moved to new function main().

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from lark import Lark
 from src.assembler import process
 
 
-with open('./grammar/lc3_assembly.lark', 'r') as file:
-    lc3_asm_grammar = file.read()
+def main() -> None:
+    with open('./grammar/lc3_assembly.lark', 'r') as file:
+        lc3_asm_grammar = file.read()
 
-with open('./examples/hello_world.asm', 'r') as file:
-    program = file.read()
+    with open('./examples/hello_world.asm', 'r') as file:
+        program = file.read()
 
-lark = Lark(lc3_asm_grammar)
+    lark = Lark(lc3_asm_grammar)
 
-memory = process(lark.parse(program))
-byte_memory = bytearray([e for m in memory for e in (m & 0xff, (m & 0xff00) >> 8)])
+    memory = process(lark.parse(program))
+    byte_memory = bytearray([e for m in memory for e in (m & 0xff, (m & 0xff00) >> 8)])
 
-with open('./output_programms/hello_world.raw', 'wb') as file:
-    file.write(byte_memory)
+    with open('./output_programms/hello_world.raw', 'wb') as file:
+        file.write(byte_memory)
+
+
+if __name__ == '__main__':
+    main()
