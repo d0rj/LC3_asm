23fcf8057af8736e5dde34ec83cb9bd326ad337b
d0rj <hanaan@internet.ru>
2021-07-24 11:49:45 +0300

    Changed message creation function signatures; added another tests for 'arguments_matched' function; code style.

--- src/typechecking.py
+++ src/typechecking.py
@@ -3,36 +3,44 @@ from typing import List, Tuple
 from .asm_code.argument import Argument
 
 
-def _argument_type_missmatch_message(instruction_name: str, required: str) -> str:
-    return f'''\'{instruction_name.upper()}\' instruction arguments must have types {required}.'''
+def _type_missmatch_message(instr: str, types: List[str]) -> str:
+    return f"\'{instr.upper()}\' instruction arguments must have types {types}."
 
 
-def _argument_count_missmatch_message(instruction_name: str, required: int, passed: int) -> str:
-    return f'\'{instruction_name.upper()}\' instruction must have {required} arguments, but {passed} was gived.'
+def _count_missmatch_message(instr: str, required: int, passed: int) -> str:
+    return f'\'{instr.upper()}\' instruction must have {required} arguments, but {passed} was gived.'
 
 
-def arguments_matched(arguments: List[Argument], types: List[str], instr: str) -> Tuple[bool, str]:
-    if len(arguments) != len(types):
-        return False, _argument_count_missmatch_message(instr, len(types), len(arguments))
+def arguments_matched(
+        args: List[Argument],
+        types: List[str],
+        instr: str
+        ) -> Tuple[bool, str]:
+    if len(args) != len(types):
+        return False, _count_missmatch_message(instr, len(types), len(args))
 
-    all_matched = all(arg.type_ == _type for arg, _type in zip(arguments, types))
+    all_matched = all(arg.type_ == type for arg, type in zip(args, types))
 
     if not all_matched:
-        return False, _argument_type_missmatch_message(instr, str(types).replace('[', '').replace(']', ''))
+        return False, _type_missmatch_message(instr, types)
 
     return True, str()
 
 
-def arguments_matched_any(arguments: List[Argument], types: List[List[str]], instr: str) -> Tuple[int, str]:
-    for i, _types in enumerate(types):
-        matched, message = arguments_matched(arguments, _types, instr)
+def arguments_matched_any(
+        args: List[Argument],
+        list_types: List[List[str]],
+        instr: str
+        ) -> Tuple[int, str]:
+    for i, types in enumerate(list_types):
+        matched, message = arguments_matched(args, types, instr)
         if matched:
             return i, str()
 
     return -1, message
 
 
-def required_argument_types(arguments: List[Argument], types: List[str], instr: str):
-    matched, message = arguments_matched(arguments, types, instr)
+def required_argument_types(args: List[Argument], types: List[str], instr: str):
+    matched, message = arguments_matched(args, types, instr)
     if not matched:
         raise SyntaxError(message)
--- tests/test_typechecking.py
+++ tests/test_typechecking.py
@@ -1,5 +1,6 @@
 from src.asm_code.argument import Argument
-from src.typechecking import arguments_matched
+from src.typechecking import arguments_matched,\
+    _count_missmatch_message, _type_missmatch_message
 from src.utils.lc3_constants import TokenType as TT
 
 
@@ -16,3 +17,21 @@ def test_arguments_matched_simple():
     args = [Argument('joke', 'ha'), Argument('math', 3.14)]
     pattern = ['joke', 'math']
     assert arguments_matched(args, pattern, 'unsupported') == (True, str())
+
+
+def test_arguments_matched_unmatch():
+    for i in range(3):
+        args = [Argument(TT.NUMBER, 1)] * i
+        pattern = [TT.NUMBER] * (i + 1)
+        assert arguments_matched(args, pattern, 'count') ==\
+            (False, _count_missmatch_message('count', len(pattern), len(args)))
+
+    args = [Argument(TT.NUMBER, 10)]
+    pattern = [TT.STRING]
+    assert arguments_matched(args, pattern, 'types') ==\
+        (False, _type_missmatch_message('types', pattern))
+
+    args = [Argument(TT.NUMBER, 1), Argument(TT.STRING, 'test')]
+    pattern = [TT.STRING, TT.NUMBER]
+    assert arguments_matched(args, pattern, 'types_swap') ==\
+        (False, _type_missmatch_message('types_swap', pattern))
