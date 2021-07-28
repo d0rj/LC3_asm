8e5510b8f37e149e5b39f3d89acba5b8a53097dd
d0rj <hanaan@internet.ru>
2021-07-28 23:47:56 +0300

    docs: add documentation for typechecking function without errors raise

--- src/typechecking.py
+++ src/typechecking.py
@@ -15,6 +15,31 @@ def _count_missmatch_message(instr: str, required: int, passed: int) -> str:
 def arguments_matched(
         args: List[Argument], types: List[str], instr: str
         ) -> Tuple[bool, str]:
+    """
+    Returns flag that arguments matched the types pattern
+
+    Args:
+        args (List[Argument]): Actual arguments to check
+        types (List[str]): Types pattern to ckeck
+        instr (str): Instruction name. Required for error message
+
+    Returns:
+        Tuple[bool, str]: First element - flag that shows matching result
+        (True if types are the same as in `types` pattern argument).
+        Second element - error message. It will be empty if first element
+        is True
+
+    Examples:
+        >>> args = [Argument('number', 1), Argument('number', 2)]
+        >>> types = ['number', 'number']
+        >>> arguments_matched(args, types, 'example')
+        (True, '')
+
+        >>> args = [Argument('number', 1)]
+        >>> types = ['string']
+        >>> arguments_matched(args, types, 'example2')
+        (False, "'EXAMPLE2' instruction arguments must have types ['string'].")
+    """
     if len(args) != len(types):
         return False, _count_missmatch_message(instr, len(types), len(args))
 
@@ -29,6 +54,32 @@ def arguments_matched(
 def arguments_matched_any(
         args: List[Argument], list_types: List[List[str]], instr: str
         ) -> Tuple[int, str]:
+    """
+    Returns flag that arguments matched any of types pattern in given list
+    of patterns
+
+    Args:
+        args (List[Argument]): Actual arguments to check
+        list_types (List[List[str]]): List of types patterns to check
+        instr (str): Instruction name. Required for error message
+
+    Returns:
+        Tuple[int, str]: First element - index of matched types pattern in
+        patterns list (-1 if no matched pattern). Second element - error
+        message. It will be empty if first element is not equal -1. It will
+        contain message for last pattern check
+
+    Examples:
+        >>> args = [Argument('number', 1), Argument('string', 'str')]
+        >>> patterns = [['number', 'number'], ['number', 'string'], ['string']]
+        >>> arguments_matched_any(args, patterns, 'example')
+        (1, '')
+
+        >>> args = [Argument('number', 1), Argument('string', 'str')]
+        >>> patterns = [['string', 'number'], ['string']]
+        >>> arguments_matched_any(args, patterns, 'example2')
+        (-1, "'EXAMPLE2' instruction must have 1 arguments, but 2 was gived.")
+    """
     for i, types in enumerate(list_types):
         matched, message = arguments_matched(args, types, instr)
         if matched:
