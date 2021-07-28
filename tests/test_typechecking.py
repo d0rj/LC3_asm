5c00143f686163cd633aaa606841e9aaf4b4dff0
d0rj <hanaan@internet.ru>
2021-07-28 23:00:17 +0300

    test: add tests for multiple typechecks function

--- tests/test_typechecking.py
+++ tests/test_typechecking.py
@@ -1,7 +1,8 @@
 import pytest
 
 from src.asm_code.argument import Argument
-from src.typechecking import arguments_matched, required_argument_types,\
+from src.typechecking import arguments_matched, arguments_matched_any,\
+    required_argument_types,\
     _count_missmatch_message, _type_missmatch_message
 from src.utils.lc3_constants import TokenType as TT
 
@@ -37,6 +38,42 @@ def test_arguments_matched_unmatch():
     pattern = [TT.STRING, TT.NUMBER]
     assert arguments_matched(args, pattern, 'types_swap') ==\
         (False, _type_missmatch_message('types_swap', pattern))
+
+
+def test_arguments_matched_any_simple():
+    for i in range(3):
+        args = [Argument(TT.NUMBER, 10)] * i
+        pattern = [[TT.NUMBER] * i]
+        assert arguments_matched_any(args, pattern, 'count') == (0, str())
+
+    args = [Argument(TT.NUMBER, 10), Argument(TT.STRING, 'strr')]
+    pattern = [[TT.NUMBER, TT.STRING]]
+    assert arguments_matched_any(args, pattern, 'different_types') == (0, str())
+
+    args = [Argument(TT.NUMBER, 10), Argument(TT.NUMBER, 20)]
+    pattern = [
+        [TT.STRING, TT.STRING],
+        [TT.NUMBER, TT.NUMBER]
+    ]
+    assert arguments_matched_any(args, pattern, 'many') == (1, str())
+
+
+def test_arguments_matched_any_unmatch():
+    args = [Argument(TT.NUMBER, 10), Argument(TT.NUMBER, 20)]
+    pattern = [
+        [TT.NUMBER],
+        [TT.NUMBER, TT.NUMBER, TT.NUMBER]
+    ]
+    assert arguments_matched_any(args, pattern, 'many') ==\
+        (-1, _count_missmatch_message('many', len(pattern[1]), len(args)))
+
+    args = [Argument(TT.NUMBER, 10), Argument(TT.NUMBER, 20)]
+    pattern = [
+        [TT.STRING, TT.STRING],
+        [TT.STRING, TT.NUMBER]
+    ]
+    assert arguments_matched_any(args, pattern, 'many') ==\
+        (-1, _type_missmatch_message('many', pattern[1]))
 
 
 def test_required_argument():
