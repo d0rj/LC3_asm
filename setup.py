9efa0fc7813bc25ab53685f2217a964d7a352858
d0rj <hanaan@internet.ru>
2021-08-03 09:05:28 +0300

    build: add `dataclasses` setup for elder versions
    Now module should be runned by Python <= 3.6.
    
    Added CI tests check for elder versions of Python.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+import sys
 from setuptools import setup, find_packages
 
 
 with open('./requirements.txt') as file:
     requires = file.readlines()
+
+if sys.version_info.minor < 7:
+    requires.append('dataclasses')
 
 setup(
     name="lc3_asm",
